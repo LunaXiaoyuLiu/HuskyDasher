3d257608234ff553588c709b5430aa3bf3fdb548
LunaXiaoyuLiu <rainielvictory@gmail.com>
2025-07-02 03:44:14 -0700

    database setup

--- src/main/java/com/luna/huskydasher/hello/HelloController.java
+++ src/main/java/com/luna/huskydasher/hello/HelloController.java
@@ -1,20 +1,33 @@
 package com.luna.huskydasher.hello;
 
+import net.datafaker.Faker;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    public Person sayHello() {
-        return new Person(
-                "John Doe",
-                "Laioffer",
-                new Address("123 Main St", "Springfield", "IL", "USA"),
-                new Book("Effective Java", "Joshua Bloch")
-        );
+    public Person sayHello( @RequestParam(required = false) String name ) {
+        if (name == null) {
+            name = "Guest";
+        }
+        Faker faker = new Faker();
+        String company = faker.company().name();
+        String street = faker.address().streetAddress();
+        String city = faker.address().city();
+        String state = faker.address().state();
+        String bookTitle = faker.book().title();
+        String bookAuthor = faker.book().author();
+
+
+        return new Person(name, company, new Address(street, city, state, null), new Book(bookTitle, bookAuthor));
+
+
     }
+
+
 }
 
 
