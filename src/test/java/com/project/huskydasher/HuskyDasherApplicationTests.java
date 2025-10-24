bc278b20468ebe0a4c8d1878acd1a87d7bf3b908
Xiaoyu Liu <xiaoyuliu@Mac.lan>
2025-10-23 22:51:22 -0400

    Initial commit

---
+++ src/test/java/com/project/huskydasher/HuskyDasherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.huskydasher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuskyDasherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
